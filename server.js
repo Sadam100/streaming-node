91b028712aed729e0285ea06ae94fce5620e3c3c
sadamhussain <sadamhussain100@gmail.com>
2023-03-12 15:59:06 +0500

    frontend build

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const path = require('path')
+const fs = require('fs')
 
 const app = express()
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-})
+app.use(express.static(path.join(__dirname, "./build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
+// app.get('/', (req, res) => {
+//     res.send("Hello World");
+// })
 
 app.listen(port, async () => {
     try {
