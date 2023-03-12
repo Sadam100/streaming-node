c3d83b7de2ef423ef22aeacb112ba2753e0bcb15
sadamhussain <sadamhussain100@gmail.com>
2023-03-12 16:04:14 +0500

    video link

--- server.js
+++ server.js
@@ -6,6 +6,49 @@ const app = express()
 
 const port = process.env.PORT || 3000;
 
+const videoFileMap={
+    'cdn':'videos/cdn.mp4',
+    'generate-pass':'videos/generate-pass.mp4',
+    'get-post':'videos/get-post.mp4',
+}
+
+app.get('/videos/:filename', (req, res)=>{
+    const fileName = req.params.filename;
+    const filePath = videoFileMap[fileName]
+    if(!filePath){
+        return res.status(404).send('File not found')
+    }
+
+    const stat = fs.statSync(filePath);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+
+    if(range){
+        const parts = range.replace(/bytes=/, '').split('-')
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+        const chunksize = end - start + 1;
+        const file = fs.createReadStream(filePath, {start, end});
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4'
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
+    }
+    else{
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4'
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(filePath).pipe(res)
+    }
+})
+
 app.use(express.static(path.join(__dirname, "./build")));
 app.get("*", function (_, res) {
   res.sendFile(
